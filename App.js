0f4ca21957b1b1e1b95f2ab15cc567211509bd6d
adtmr <anjdeitmer@gmail.com>
2022-12-02 13:48:04 +0100

    Update App.js

--- App.js
+++ App.js
@@ -1,7 +1,23 @@
 import { StatusBar } from 'expo-status-bar';
 import { StyleSheet, Text, View } from 'react-native';
+import axios from 'axios';
 
+const endpoint = 'https://www.eventbriteapi.com/v3/'
+const token = 'BZW2PTENUJHKKSJDM5WA'
+// subcategories/16006
+// https://www.eventbriteapi.com/v3/users/me/?token=BZW2PTENUJHKKSJDM5WA
+// categories/
+// subcategories/16006/
 export default function App() {
+  axios({
+    method: 'get',
+    url: `${endpoint}events/subcategories/16006/?token=${token}`,
+  }).then((response) => {
+    console.log('?????????????', response)
+  })
+  .catch(error => console.log('error', error))
+
+
   return (
     <View style={styles.container}>
       <Text>Open up App.js to start working on your app!</Text>
@@ -9,6 +25,9 @@ export default function App() {
     </View>
   );
 }
+
+
+
 
 const styles = StyleSheet.create({
   container: {
